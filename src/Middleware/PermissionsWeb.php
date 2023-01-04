30a4910b1fd84a2c5ff511bfb28103cdcc0fc8d0
Zain ul abdin <zainburfat112@gmail.com>
2023-01-04 13:01:59 +0500

    update

--- src/Middleware/PermissionsWeb.php
+++ src/Middleware/PermissionsWeb.php
@@ -4,14 +4,22 @@ namespace Zainburfat\Rbac\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class PermissionsWeb
 {
     public function handle(Request $request, Closure $next)
     {
-        if (!$request->user()->hasPermissionTo($request->route()->getName()))
-            if (!$request->user()->hasDirectPermissionTo($request->route()->getName()))
-                abort(403);
+        $temp = Str::of($request->route()->getActionName())->afterLast('\\')->split('[@]');
+        $scope = (string)Str::of($temp[0])->lower()->remove('controller')->append(' '.$temp[1])->slug('.');
+
+        if (!$request->user()->tokenCan($scope)) {
+            abort(403);
+        }
+
+        // if (!$request->user()->hasPermissionTo($request->route()->getName()))
+        //     if (!$request->user()->hasDirectPermissionTo($request->route()->getName()))
+        //         abort(403);
 
         return $next($request);
     }
