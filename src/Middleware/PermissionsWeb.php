a636f8f2450972aaf42664fa2f4f722f3caf3ae9
Zain ul abdin <zainburfat112@gmail.com>
2023-01-04 13:21:59 +0500

    update

--- src/Middleware/PermissionsWeb.php
+++ src/Middleware/PermissionsWeb.php
@@ -11,15 +11,13 @@ class PermissionsWeb
     public function handle(Request $request, Closure $next)
     {
         $temp = Str::of($request->route()->getActionName())->afterLast('\\')->split('[@]');
-        $scope = (string)Str::of($temp[0])->lower()->remove('controller')->append(' '.$temp[1])->slug('.');
+        $permission = (string)Str::of($temp[0])->lower()->remove('controller')->append(' ' . $temp[1])->slug('.');
 
-        if (!$request->user()->tokenCan($scope)) {
-            abort(403);
+        if (!$request->user()->hasPermissionTo($permission)) {
+            if (!$request->user()->hasDirectPermissionTo($permission)) {
+                abort(403);
+            }
         }
-
-        // if (!$request->user()->hasPermissionTo($request->route()->getName()))
-        //     if (!$request->user()->hasDirectPermissionTo($request->route()->getName()))
-        //         abort(403);
 
         return $next($request);
     }
